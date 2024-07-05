2b6b34255649e28141315a943ff588002017b0fd
david-blg <david.stack.dev@gmail.com>
2024-07-05 20:43:56 +0200

    chore: Move updater function to App.tsx and update tauri dependency features

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import AppLayout from "./components/layouts/AppLayout";
 import AppRoutes from "./routes/AppRoutes";
 import { invoke } from '@tauri-apps/api/tauri'
-import { checkUpdate } from "@tauri-apps/api/updater";
-// import { setupUpdater } from "./update/updater";
+import { setupUpdater } from "./update/updater";
 
 function App() {
-  const updateChecked = useRef(false);
+
   useEffect(() => {
     // document.addEventListener('contextmenu', event => event.preventDefault());
 
     const closeSplashscreenTimeout = setTimeout(async () => {
       await invoke("close_splashscreen");
-
       window.clearTimeout(closeSplashscreenTimeout);
     }, 1000);
 
@@ -24,21 +22,19 @@ function App() {
 
 
   useEffect(() => {
-    async function checkForUpdates() {
-      if (!updateChecked.current) {
-        try {
-          await checkUpdate();
-        } catch (error) {
-          console.error('Error checking for updates:', error);
-        }
-        updateChecked.current = true;
-      }
-    }
+    let unlistenUpdater: (() => void) | undefined;
 
-    checkForUpdates();
+    setupUpdater().then(unlisten => {
+      unlistenUpdater = unlisten;
+    });
+
+    return () => {
+      if (unlistenUpdater) {
+        unlistenUpdater();
+      }
+    };
   }, []);
 
-  
   return (
     <AppLayout>
       <AppRoutes />
--- src/update/updater.ts
+++ src/update/updater.ts
@@ -1,25 +1,30 @@
-import { checkUpdate, installUpdate, onUpdaterEvent } from '@tauri-apps/api/updater'
-import { relaunch } from '@tauri-apps/api/process'
+import { checkUpdate, installUpdate, onUpdaterEvent } from '@tauri-apps/api/updater';
+import { relaunch } from '@tauri-apps/api/process';
+
+let isUpdaterSetup = false;
 
 export async function setupUpdater() {
+    if (isUpdaterSetup) return;
+    isUpdaterSetup = true;
+
     const unlisten = await onUpdaterEvent(({ error, status }) => {
-        console.log('Updater event', error, status)
-    })
+        console.log('Updater event', error, status);
+    });
 
     try {
-        const { shouldUpdate, manifest } = await checkUpdate()
+        const { shouldUpdate, manifest } = await checkUpdate();
 
         if (shouldUpdate) {
             console.log(
                 `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-            )
+            );
 
-            await installUpdate()
-            await relaunch()
+            await installUpdate();
+            await relaunch();
         }
     } catch (error) {
-        console.error('Error during update check:', error)
+        console.error('Error during update check:', error);
     }
 
-    return unlisten
+    return unlisten;
 }
