d6390c2b3bc72f586c9f1af3d0c3a2c3c3b5b783
jer-nc <rejdev24@gmail.com>
2024-09-01 21:11:10 +0200

    fix: Update S3 CLI configuration for profile in setS3cliConfig.ts & bucket location farm deploy

--- src/cli-functions/deploy/s3-codebuild/getS3Codebuild.ts
+++ src/cli-functions/deploy/s3-codebuild/getS3Codebuild.ts
@@ -28,10 +28,10 @@ export async function getS3Codebuild(region: string, profile: string) {
         console.log('Bucket Names:', bucketNames);
 
         if (bucketNames.length === 0) {
-            return { exists: false, bucketName: null };
+            return { exists: false, bucketName: null, region: null };
         }
 
-        // Ordenar los nombres de los buckets por el timestamp
+        // sort the bucket names by timestamp
         const sortedBucketNames = bucketNames.sort((a, b) => {
             const timestampA = a.split('-').pop();
             const timestampB = b.split('-').pop();
@@ -40,7 +40,35 @@ export async function getS3Codebuild(region: string, profile: string) {
 
         const mostRecentBucketName = sortedBucketNames[0];
 
-        return { exists: true, bucketName: mostRecentBucketName };
+        // Get the location of the bucket
+        const getBucketLocationCommand = new Command('aws-cli', [
+            "s3api",
+            "get-bucket-location",
+            "--bucket", mostRecentBucketName,
+            "--profile", profile
+        ]);
+
+        const bucketLocationOutput = await getBucketLocationCommand.execute();
+        const bucketLocationStderr = bucketLocationOutput.stderr?.toString() || '';
+
+        if (bucketLocationOutput.code !== 0) {
+            console.error(`Failed to get bucket location with code ${bucketLocationOutput.code}`);
+            console.error(`stderr: ${bucketLocationStderr}`);
+            throw new Error(bucketLocationStderr);
+        }
+
+        const bucketLocationJson = bucketLocationOutput.stdout?.toString().trim();
+        const bucketLocationData = JSON.parse(bucketLocationJson || '{}');
+        const bucketRegion = bucketLocationData.LocationConstraint || 'us-east-1';
+
+        // Check if the bucket is in the same region
+        const isSameRegion = bucketRegion === region;
+
+        return {
+            exists: isSameRegion,
+            bucketName: isSameRegion ? mostRecentBucketName : null,
+            region: isSameRegion ? bucketRegion : null
+        };
 
     } catch (error) {
         if (error instanceof Error) {
